fd3e4fc0cda6de8d193dbe48e232c12ac138347f
Anthony Speece <anthonyspeece@gmail.com>
2022-11-05 13:10:45 -0500

    add functionality to server.js for get,post,delete routes

--- Develop/routes/notes.js
+++
@@ -1,9 +0,0 @@
-const notes = require("express").Router();
-const { v4: uuidv4 } = require("uuid");
-const {
-  readFromFile,
-  readAndAppend,
-  writeToFile,
-} = require("../helpers/fsUtils");
-
-module.exports = notes;
--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,10 @@
+//Global Variables
 const express = require("express");
+const fs = require("fs");
 const path = require("path");
-const { clog } = require('./middleware/clog');
-const api = require('./routes/index');
+const { clog } = require("./middleware/clog");
+
+const { v4: uuidv4 } = require("uuid");
 
 const PORT = process.env.PORT || 3001;
 
@@ -13,21 +16,56 @@ app.use(clog);
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // GET Route for homepage
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
 );
 
 // GET Route for the Notes page
-app.get('/notes',(req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
 
+// GET Route to read the db.json file and display the notes
+app.get("/api/notes", (req, res) => {
+  fs.readFile("./db/db.json", "utf-8", (err, data) => {
+    const notes = JSON.parse(data);
+    res.json(notes);
+  });
+});
 
+// POST Route to be able to add to the db.json and diplay the newly added note
+app.post("/api/notes", (req, res) => {
+  fs.readFile("./db/db.json", "utf-8", (err, data) => {
+    const notes = JSON.parse(data);
+    notes.push({
+      title: req.body.title,
+      text: req.body.text,
+      id: uuidv4(),
+    });
+    fs.writeFile("./db/db.json", JSON.stringify(notes), (err) => {
+      res.json(notes);
+    });
+  });
+});
+
+//Delete Route to be able to remove a note by its unique ID
+app.delete("/api/notes/:id", (req, res) => {
+  fs.readFile("./db/db.json", "utf-8", (err, data) => {
+    const notes = JSON.parse(data);
+    const updatedNotes = notes.filter((note) => {
+      return note.id != req.params.id;
+    });
+    fs.writeFile("./db/db.json", JSON.stringify(updatedNotes), (err) => {
+      res.json(updatedNotes);
+    });
+  });
+});
+
+//Listen to the User PORT
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
